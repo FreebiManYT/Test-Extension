3621706cb7669843e14e3207619a298bab30a090
FreebiManYT <103787543+FreebiManYT@users.noreply.github.com>
2022-10-08 22:41:59 -0400

    Update setup-user.js

--- setup-user.js
+++ setup-user.js
@@ -1,7 +1,9 @@
-var user = document.getElementsByClassName('text-overflow age-bracket-label-username font-caption-header')[0]
-var link = document.getElementsByClassName('text-link dynamic-overflow-container')[0].href.replace('https://www.roblox.com/users/','').replace('/profile','')
+$(document).ready(function(){
+  var user = document.getElementsByClassName('text-overflow age-bracket-label-username font-caption-header')[0]
+  var link = document.getElementsByClassName('text-link dynamic-overflow-container')[0].href.replace('https://www.roblox.com/users/','').replace('/profile','')
 
-fetch('https://users.roblox.com/v1/users/'+link, {
-  method: "GET",
-  redirect: "follow"
-}).then((r) => r.json()).then((data) => console.log(data)).then((error) => console.log('error', error));
+  fetch('https://users.roblox.com/v1/users/'+link, {
+    method: "GET",
+    redirect: "follow"
+  }).then((r) => r.json()).then((data) => console.log(data)).then((error) => console.log('error', error));
+});
